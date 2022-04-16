26c0fa7cc8373e4bbf2e6af5ecf63c3c8f736c2f
tkla <kennyla95@gmail.com>
2022-04-16 04:39:45 -0700

    Add voiceStateUpdate event and debug console statements

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-require('dotenv').config(); //initialize dotenv
-const axios = require('axios');
+import dotenv from 'dotenv'
+dotenv.config()
+import axios from 'axios';
 import Discord from 'discord.js'; //import discord.js
 import { Client, Intents } from 'discord.js';
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-//make sure this line is the last line
+client.on('voiceStateUpdate', (oldState, newState) => {
+  // Ignore bot and non Zhi users
+  if (oldState.member.user.bot || oldState.member.user.username !== 'Fu Manchu') return;
+
+  // Determine Zhi state
+  if (!oldState.channelId) {
+    console.log("He's here");
+  }
+})
+
 client.login(process.env.CLIENT_TOKEN); //login bot using token
